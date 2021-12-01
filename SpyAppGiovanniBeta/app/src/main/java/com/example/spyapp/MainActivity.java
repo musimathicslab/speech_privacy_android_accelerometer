8568d4e01cd12c137ff09d575c92d06c985d6038
Giovanni Rapa <48215666+Givaa@users.noreply.github.com>
2021-12-01 20:02:14 +0100

    upload entire beta of the app

---
+++ SpyAppGiovanniBeta/app/src/main/java/com/example/spyapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.spyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void go(View view) {
+        System.out.println("clicked");
+    }
+
+    public void test(View view) {
+        System.out.println("working!!");
+    }
+}
