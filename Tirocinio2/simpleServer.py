93150db86827196882dd7d6b65c408e0c8790696
Michele-iodice <72759068+Michele-iodice@users.noreply.github.com>
2022-01-14 18:46:32 +0100

    Update simpleServer.py

--- Tirocinio2/simpleServer.py
+++ Tirocinio2/simpleServer.py
@@ -5,14 +5,21 @@ import numpy as np
 import scipy.interpolate
 from scipy import signal
 import tensorflow as tf
-import EvolutiveAlgoritm
 
-def highPassFilter(x, cutoff):
-    fs = len(x)
+
+def high_pass_filter(x_filter, cutoff):
+    """
+      High Pass Filter
+      it divide the signal of speech from that of movements
+      :param x_filter: one of the axis of the data in vector form
+      :param cutoff: integer that represent limit of the cutoff
+      :return: the data with cutoff application
+    """
+    fs = len(x_filter)
     nyq = 0.5 * fs
     normal_cutoff = cutoff / nyq
 
-    f, t, zxx = signal.stft(x)
+    f, t, zxx = signal.stft(x_filter)
 
     for n in range(0, len(f)):
         if f[n] <= normal_cutoff:
@@ -21,14 +28,15 @@ def highPassFilter(x, cutoff):
     a, rec = signal.istft(zxx)
     rec = list(rec)
 
-    '''if (len(rec))>=fs:
-        i=len(rec)-fs
-        for n in range(0,i):
-            rec.pop()'''
-
     return rec
 
+
 def get_spectrogram(waveform):
+    """
+           Function for generate a spectrogram from a signal
+           :param waveform: the signal
+           :return: the spectrogram of signal
+        """
 
     spectrogram = tf.signal.stft(
         waveform, frame_length=255, frame_step=128)
@@ -37,13 +45,16 @@ def get_spectrogram(waveform):
 
     return spectrogram
 
+
 def interpolate_1d_vector(vector, factor):
     """
-    Interpolate, i.e. upsample, a given 1D vector by a specific interpolation factor.
-    :param vector: 1D data vector
-    :param factor: factor for interpolation (must be integer)
-    :return: interpolated 1D vector by a given factor
-    """
+        Function of interpolation
+         Interpolate, i.e. up sample, a given 1D vector by a specific interpolation factor.
+         :param vector: 1D data vector
+         :param factor: factor for interpolation (must be integer)
+         :return: interpolated 1D vector by a given factor
+     """
+    
     x = np.arange(np.size(vector))
     y = vector
     f = scipy.interpolate.interp1d(x, y)
@@ -59,7 +70,14 @@ def interpolate_1d_vector(vector, factor):
     return y_interpolated
 
 
-def setElement(file, dir):
+def set_element(file, dir):
+    """
+         Signal load and pre-elaboration
+         :param file: name of the file to be processed
+         :param dir : name of the file's directory
+         :return: data vector that represent the signal
+      """
+    
     x = []
     y = []
     z = []
@@ -82,9 +100,9 @@ def setElement(file, dir):
         y = list(interpolate_1d_vector(y, 3))
         z = list(interpolate_1d_vector(z, 3))
 
-        x = highPassFilter(x, 80)
-        y = highPassFilter(y, 80)
-        z = highPassFilter(z, 80)
+        x = high_pass_filter(x, 80)
+        y = high_pass_filter(y, 80)
+        z = high_pass_filter(z, 80)
 
         x = get_spectrogram(x)
         y = get_spectrogram(y)
@@ -101,13 +119,18 @@ def setElement(file, dir):
         pil_img = tf.keras.preprocessing.image.array_to_img(data)
         data = tf.keras.preprocessing.image.img_to_array(pil_img)
         data = tf.image.resize(data, [224, 224])
-        print(f'{dir} {file} : TERMINATO')
+        print(f'{dir} {file} : End')
     return data
 
 
-def listDir(directory, file):
+def list_dir(directory, file):
+    """
+          Cycle of signal elaboration
+           :return: data vector that represent the signal
+        """
+    
     data = []
-    x = setElement(file, directory)
+    x = set_element(file, directory)
     if len(x) > 0:
         data.append(x)
     data = np.asarray(data)
@@ -120,20 +143,19 @@ app = flask.Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
 
-    #RICEZIONE DEL FILE
+    # FILE Reception
     fileCsv = flask.request.files['image']
 
     print("\nReceived file : " + fileCsv.filename)
-    #SALVATAGGIO DEL FILE
+    # FILE save
     fileCsv.save('ServerFile/'+fileCsv.filename)
-    #ELABORAZIONE DEL FILE
-    Data = listDir('ServerFile', fileCsv.filename)
-    #CARICAMENTO MODELLO
-    loaded_model = tf.keras.models.load_model('ReteSpeechModels/Result_Test24')
+    # FILE elaboration
+    Data = list_dir('ServerFile', fileCsv.filename)
+    # Model load
+    loaded_model = tf.keras.models.load_model('ReteSpeechModels/Result_Test34')
     y_pred = np.argmax(loaded_model.predict(Data), axis=1)
 
-
-    #INVIO RISULTATI
+    # Sending result
     print(f'SPEECH PRONUNCIATA :{y_pred}')
     risultato = f'{y_pred}'
 
